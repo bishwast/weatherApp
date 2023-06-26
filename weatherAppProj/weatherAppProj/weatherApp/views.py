434eb6714f1132325757a2fef0bf33d30f7b7ac4
Sunil T Bishwakarma <bishwast77@gmail.com>
2023-06-26 13:30:52 -0600

    Weather app part 1
    Up to now,
    Created proj and app with django-admin
    added index.html code for front end
    added weatherApp to Installed App list in settings.py main project
    added DIR path to the folder that contains html templates
    In weatherApp folder created a urls.py and added irl patterns path to include index for views
    created a simple func in views.py to render a POST request, collect data for attributes from index.html front-end
    finally included urls.py path to main weatherapp proj folder > urls.py
    applied csrf token prop to the index.html and ran django server successfully.

--- weatherAppProj/weatherAppProj/weatherApp/views.py
+++ weatherAppProj/weatherAppProj/weatherApp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    ## Collect the data from user input
+    if request.method == 'POST':        ## Sending a form
+        city = request.POST['city']     ## Colelct a form for city
+    else:
+        city = ''
+    return render(request, 'index.html', {'city': city})
